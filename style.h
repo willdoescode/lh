dd0b275a82702a746dc19885be8028b22940cebf
Will Lane <williamlane923@gmail.com>
2021-09-17 09:29:03 -0700

    get rid of those warnings

--- style.h
+++ style.h
@@ -7,27 +7,27 @@
 // sequence after the bracket, and one for the m
 #define MAX_COLOR_LEN 6
 
-const static char end[] = "\033[0;0m";
-const static char bold[] = "\033[1m";
-const static char dull[] = "\033[2m";
-const static char italic[] = "\033[3m";
-const static char underline[] = "\033[4m";
-const static char inverted[] = "\033[7m";
-const static char dark_black[] = "\033[30m";
-const static char dark_red[] = "\033[31m";
-const static char dark_green[] = "\033[32m";
-const static char dark_yellow[] = "\033[33m";
-const static char dark_blue[] = "\033[34m";
-const static char dark_magenta[] = "\033[35m";
-const static char dark_cyan[] = "\033[36m";
-const static char dark_white[] = "\033[37m";
-const static char light_black[] = "\033[90m";
-const static char light_red[] = "\033[91m";
-const static char light_green[] = "\033[92m";
-const static char light_yellow[] = "\033[93m";
-const static char light_blue[] = "\033[94m";
-const static char light_magenta[] = "\033[95m";
-const static char light_cyan[] = "\033[96m";
-const static char light_white[] = "\033[97m";
+static char* end = "\033[0;0m";
+static char* bold = "\033[1m";
+static char* dull = "\033[2m";
+static char* italic = "\033[3m";
+static char* underline = "\033[4m";
+static char* inverted = "\033[7m";
+static char* dark_black = "\033[30m";
+static char* dark_red = "\033[31m";
+static char* dark_green = "\033[32m";
+static char* dark_yellow = "\033[33m";
+static char* dark_blue = "\033[34m";
+static char* dark_magenta = "\033[35m";
+static char* dark_cyan = "\033[36m";
+static char* dark_white = "\033[37m";
+static char* light_black = "\033[90m";
+static char* light_red = "\033[91m";
+static char* light_green = "\033[92m";
+static char* light_yellow = "\033[93m";
+static char* light_blue = "\033[94m";
+static char* light_magenta = "\033[95m";
+static char* light_cyan = "\033[96m";
+static char* light_white = "\033[97m";
 
 #endif
